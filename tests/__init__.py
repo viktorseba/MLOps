84a94aafb6f89dff53a6bd6a5af80d2cff6a745e
JonasMyhreSchioett <jonasschitt@gmail.com>
2025-01-13 13:06:01 +0100

    mon det virker?

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,4 @@ import sys
 sys.path.append("src")
 _TEST_ROOT = os.path.dirname(__file__)  # root of test folder
 _PROJECT_ROOT = os.path.dirname(_TEST_ROOT)  # root of project
-_PATH_DATA = os.path.join(_PROJECT_ROOT, "data")  # root of data
+_PATH_DATA = os.path.join(_PROJECT_ROOT, "data", "processed", "train_images.pt")  # root of data
